37a32b30cb5140c02dcb40004ff071adc426c697
Rafael Damasceno <rafaeldamascenowhy@gmail.com>
2023-01-29 23:30:30 -0300

    create etapasjs

---
+++ script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('.d1-1 span');
+let cargo = document.querySelector('.d1-2 span');
+let descricao = document.querySelector('.d1-4');
+let aviso = document.querySelector('.d2');
+let lateral = document.querySelect('.d1-right');
+let numero = document.querySelector('.d1-3');
